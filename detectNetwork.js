1f7f7445e78ffca78f6706402bb5a5700d7d2154
Ephraim Glick <ephraim.glick@gmail.com>
2017-07-28 00:14:12 -0700

    Provide complete solutions to testbuilder exercise

--- detectNetwork.js
+++ detectNetwork.js
@@ -7,12 +7,63 @@
 //   1. The first few numbers (called the prefix)
 //   2. The number of digits in the number (called the length)
 
-var detectNetwork = function(cardNumber) {
+var detectNetwork = function(cn) {
   // Note: `cardNumber` will always be a string
   // The Diner's Club network always starts with a 38 or 39 and is 14 digits long
   // The American Express network always starts with a 34 or 37 and is 15 digits long
 
   // Once you've read this, go ahead and try to implement this function, then return to the console.
+
+  let len = cn.length;
+
+	if (len === 15 && ['34', '37'].includes(cn.slice(0,2))) { 
+		return "American Express" 
+	} 
+
+	else if (len === 14 && ['38', '39'].includes(cn.slice(0,2))) {
+		return 'Diner\'s Club'
+	} 
+
+    else if (
+    	  (
+    		[4903, 4905, 4911, 4936, 6333, 6759].includes(Number(cn.slice(0,4))) 
+     	    || ['564182', '633110'].includes(cn.slice(0,6))
+     	  ) &&
+     	    [16, 18, 19].includes(len)
+     	){
+     	return 'Switch'
+    }
+
+	else if (cn[0] === '4' && [13, 16, 19].includes(len)) {
+		return "Visa"
+	} 
+
+	else if (len === 16 && Number(cn.slice(0,2)) > 50 && Number(cn.slice(0,2)) < 56) {
+		return "MasterCard"
+
+	}	
+
+	else if ([16, 19].includes(len) && (cn.slice(0,2) === '65' || cn.slice(0,4) === '6011' || 
+		(Number(cn.slice(0,3)) > 643 && Number(cn.slice(0,3)) < 650))) {
+		return "Discover"
+	}
+
+	else if ((len > 11 && len < 20) && ['5018', '5020', '5038', '6304'].includes(cn.slice(0,4))) {
+		return "Maestro"
+	}
+
+	else if (len > 15 && len < 20 && (
+		((Number(cn.slice(0,3)) > 623) && Number(cn.slice(0,3)) < 627) ||
+        ((Number(cn.slice(0,4)) > 6281) && Number(cn.slice(0,4)) < 6289) ||
+        ((Number(cn.slice(0,6)) > 622125) && Number(cn.slice(0,6)) < 622926) )
+	  ) { 
+        return 'China UnionPay' 
+    }
+
+    else {
+    	return 'Invalid card number'
+    }
+    
 };
 
 
--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -8,52 +8,51 @@
 // different value.
 var FILL_ME_IN = 'Fill this value in';
  
-describe('Introduction to Mocha Tests - READ ME FIRST', function() {
-  // A Mocha test is just a function!
-  // If the function throws an error when run, it fails.
-  // If it doesn't throw an error when run, it doesn't fail. 
-  // To read more about mocha, visit mochajs.org
+// describe('Introduction to Mocha Tests - READ ME FIRST', function() {
+//   // A Mocha test is just a function!
+//   // If the function throws an error when run, it fails.
+//   // If it doesn't throw an error when run, it doesn't fail. 
+//   // To read more about mocha, visit mochajs.org
 
-  // Once you've read and understood this section, please comment it out. 
-  // You will not be able to proceed with a failing test. 
+//   // Once you've read and understood this section, please comment it out. 
+//   // You will not be able to proceed with a failing test. 
 
-  it('Throws an error so it fails', function() {
-    throw new Error('Delete me!');
-  });
+//   it('Throws an error so it fails', function() {
+//     throw new Error('Delete me!');
+//   });
 
-  it('Doesn\'t throw an error, so it doesn\'t fail', function() {
-    // This test doesn't really test anything at all! It will pass no matter what.
-    var even = function(num){
-      return num/2 === 0;
-    }
-    return even(10) === true;
-  });
+//   it('Doesn\'t throw an error, so it doesn\'t fail', function() {
+//     // This test doesn't really test anything at all! It will pass no matter what.
+//     var even = function(num){
+//       return num/2 === 0;
+//     }
+//     return even(10) === true;
+//   });
 
-  // In tests, we want to compare the expected behavior to the actual behavior.
-  // A test should only fail if the expected behavior doesn't match the actual.
-  it('Throws an error when expected behavior does not match actual behavior', function() {
-    var even = function(num){
-      return num/2 === 0;
-    }
+//   // In tests, we want to compare the expected behavior to the actual behavior.
+//   // A test should only fail if the expected behavior doesn't match the actual.
+//   it('Throws an error when expected behavior does not match actual behavior', function() {
+//     var even = function(num){
+//       return num/2 === 0;
+//     }
 
-    if(even(10) !== true) {
-      throw new Error('10 should be even!');
-    }
-  });
-});
+//     if(even(10) !== true) {
+//       throw new Error('10 should be even!');
+//     }
+//   });
+// });
+
 describe('Diner\'s Club', function() {
   // Be careful, tests can have bugs too...
 
   it('has a prefix of 38 and a length of 14', function() {
-    throw new Error('Delete me!');
- 
     if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
   });
 
   it('has a prefix of 39 and a length of 14', function() {
-    if (detectNetwork('3934567890123') !== 'Diner\'s Club') {
+    if (detectNetwork('39345678901234') !== 'Diner\'s Club') {
       throw new Error('Test failed');
     }
  
@@ -64,7 +63,7 @@ describe('American Express', function() {
   // It can get annoying to keep typing the if/throw, so here is a
   // helper function to throw an error if the input statement isn't true. 
   var assert = function(isTrue) {
-    if(isTrue) {
+    if(!isTrue) {
       throw new Error('Test failed');
     }
  
@@ -84,7 +83,7 @@ describe('Visa', function() {
   // Chai provides an assert that acts the same as our previous assert.
   // Search the documentation to figure out how to access it. 
   //   http://chaijs.com/
-  var assert = chai.FILL_ME_IN;
+  var assert = chai.assert;
  
 
   it('has a prefix of 4 and a length of 13', function() {
@@ -106,17 +105,19 @@ describe('MasterCard', function() {
   // If you want to know more, check out the documentation. 
   //   http://chaijs.com/api/bdd/
   var expect = chai.expect;
+  var should = chai.should();
+
  
-  it(FILL_ME_IN, function() {
-    expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
+  it('has a prefix of 51 and a length of 16', function() {
+    detectNetwork('5112345678901234').should.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
-    expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
+  it('has a prefix of 52 and a length of 16', function() {
+    detectNetwork('5212345678901234').should.equal('MasterCard');
   });
  
-  it(FILL_ME_IN, function() {
-    expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
+  it('has a prefix of 53 and a length of 16', function() {
+    detectNetwork('5312345678901234').should.equal('MasterCard');
   });
  
 
@@ -127,28 +128,97 @@ describe('MasterCard', function() {
   // and should, but that's just for learning), so once you've gotten 
   // these tests to pass using should syntax, refactor your tests to 
   // use either expect or should, but not both. 
-  var should = chai.should();
   
   it('has a prefix of 54 and a length of 16', function() {
-    detectNetwork('5412345678901234').should.equal(FILL_ME_IN);
+    detectNetwork('5412345678901234').should.equal('MasterCard');
   });
  
   it('has a prefix of 55 and a length of 16', function() {
-    detectNetwork('5512345678901234').should.equal(FILL_ME_IN);
+    detectNetwork('5512345678901234').should.equal('MasterCard');
   })
- 
+
 });
 
 describe('Discover', function() {
   // Tests without a function will be marked as "pending" and not run
   // Implement these tests (and others) and make them pass!
-  it('has a prefix of 6011 and a length of 16');
-  it('has a prefix of 6011 and a length of 19');
+
+  let prefixes = ['6011', '644', '645', '646', '647', '648', '649', '65'];
+  let lengths = [16, 19];
+  for (let i = 0; i < prefixes.length; i++) {
+    for (let j = 0; j < lengths.length; j++) {
+      let postPref = lengths[j] - prefixes[i].length;
+      let rand = Math.random().toFixed(20).toString().slice(2, postPref + 2); 
+      //console.log('For prefix ' + prefixes[i] + ' and length ' + lengths[j] + ', rand is: ' + rand );
+      (function(j) {
+        it('has a prefix of ' + prefixes[i] + ' and a length of ' + lengths[j], function() {
+        detectNetwork(prefixes[i] + rand).should.equal('Discover')
+        });
+      })(j);
+    }
+  }
+  // it('has a prefix of 6011 and a length of 16', function() {
+  //   detectNetwork('6011874298642573').should.equal('Discover');
+  // });
+  // it('has a prefix of 6011 and a length of 19', function() {
+  //   detectNetwork('6011874298642573754').should.equal('Discover');
+  // });
 });
 
 describe('Maestro', function() {
   // Write full test coverage for the Maestro card
+
+  let prefixes = ['5018', '5020', '5038', '6304'];
+  for (let i = 0; i < prefixes.length; i++) {
+    for (let len = 12; len <= 19; len++) {
+      let postPref = len - prefixes[i].length;
+      let rand = Math.random().toFixed(20).toString().slice(2, postPref + 2); 
+      (function(len) {
+        it('has a prefix of ' + prefixes[i] + ' and a length of ' + len, function() {
+        detectNetwork(prefixes[i] + rand).should.equal('Maestro')
+        });
+      })(len);
+    }
+  }
+
 });
 
-describe('should support China UnionPay')
-describe('should support Switch')
+describe('should support China UnionPay', function() {
+
+  let prefixes = ['624', '625', '626'];
+  for (let j = 6282; j <= 6288; j++) {
+    prefixes.push(j.toString())
+  };
+  for (let k = 622126; k <= 622925; k++) {
+    prefixes.push(k.toString())
+  };
+  for (let i = 0; i < prefixes.length; i++) {
+    for (let len = 16; len <= 19; len++) {
+      let postPref = len - prefixes[i].length;
+      let rand = Math.random().toFixed(20).toString().slice(2, postPref + 2); 
+      (function(len) {
+        it('has a prefix of ' + prefixes[i] + ' and a length of ' + len, function() {
+        detectNetwork(prefixes[i] + rand).should.equal('China UnionPay')
+        });
+      })(len);
+    };
+  };
+});
+
+
+describe('should support Switch', function() {
+
+  let prefixes = ['4903', '4905', '4911', '4936', '6333', '6759', '564182', '633110'];
+  let lengths = [16, 18, 19];
+   for (let i = 0; i < prefixes.length; i++) {
+     for (let j = 0; j < lengths.length; j++) {
+       let postPref = lengths[j] - prefixes[i].length;
+       let rand = Math.random().toFixed(20).toString().slice(2, postPref + 2); 
+       (function(j) {
+         it('has a prefix of ' + prefixes[i] + ' and a length of ' + lengths[j], function() {
+         detectNetwork(prefixes[i] + rand).should.equal('Switch')
+         });
+      })(j);
+     };
+   } 
+});
